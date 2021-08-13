19af1d4fd7a3ba49dc47fddc0eccf85c87f0c275
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-08-13 23:56:53 +0300

    Update SellLib_test.sol

--- SellLib_test.sol
+++ SellLib_test.sol
@@ -8,6 +8,8 @@ import "remix_tests.sol";
 // This import is required to use custom transaction context
 // Although it may fail compilation in 'Solidity Compiler' plugin
 // But it will work fine in 'Solidity Unit Testing' plugin
+// But it will work fine in 'Solidity Unit Testing' plugin
+// But it will work fine in 'Solidity Unit Testing' plugin
 import "remix_accounts.sol";
 import "../Lib/SellLib.sol";
 
